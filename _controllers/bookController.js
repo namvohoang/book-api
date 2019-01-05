cc7fcf10057525bc66b7d8d39d3010fe5ecbff40
Nam Vo <hoangnam.hcmut@gmail.com>
2019-01-05 11:24:12 +0700

    Adding Hypermedia

--- _controllers/bookController.js
+++ _controllers/bookController.js
@@ -21,14 +21,21 @@ var bookController = function(Book) {
     var query = {};
 
     if(req.query.genre) {
-        query.genre = req.query.genre;
+      query.genre = req.query.genre;
     }
     Book.find(query, function(err, books) {
         if(err) {
-            res.status(500).send(err);      
+          res.status(500).send(err);      
         }
         else {
-            res.json(books);
+          var returnBooks = [];
+          books.forEach(function(element, index, array) {
+            var newBook = element.toJSON();
+            newBook.links = {};
+            newBook.links.self = 'http://' + req.headers.host + '/api/books/' + newBook._id;
+            returnBooks.push(newBook);
+          });
+          res.json(returnBooks);
         }
     })
   }
